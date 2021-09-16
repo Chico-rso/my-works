522daadefaeeaf9a409454ab52242142d9782159
O2 <o2@Mac-mini-O2.local>
2021-09-16 17:16:56 +0300

    add popup

---
+++ src/blocks/header/header.js
@@ -0,0 +1,16 @@
+// o2.popup =
+// {
+// 	open()
+// 	{
+// 		const openPopup = document.querySelector('.header__popup');
+// 		openPopup.classList.add('active');
+// 		document.body.style.overflow = 'hidden';
+// 	},
+
+// 	close()
+// 	{
+// 		const openPopup = document.querySelector('.header__popup');
+// 		openPopup.classList.remove('active');
+// 		document.body.style.overflow = '';
+// 	}
+// }
